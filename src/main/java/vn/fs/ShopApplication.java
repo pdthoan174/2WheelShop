5944273bdb4da355cfff89eec5a1dd39616c5281
Thoan <pdthoan.20it11@vku.udn.vn>
2023-02-12 11:02:27 +0700

    a

---
+++ src/main/java/vn/fs/ShopApplication.java
@@ -0,0 +1,14 @@
+package vn.fs;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class ShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
+
+}
